8e0d9f62c68fab5b3936b4604c365edd580d0673
Adam <adamek.og@gmail.com>
2017-06-09 01:39:19 +0200

    + added IInputElement for event driven controls + added events for keyboard

--- DestroyNobots/Engine/Input/InputManager.cs
+++ DestroyNobots/Engine/Input/InputManager.cs
@@ -1,21 +1,37 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using System.Collections.Generic;
+using System;
 
 namespace DestroyNobots.Engine.Input
 {
-    public class InputManager
+    public class InputManager : IUpdateable
     {
         private Dictionary<ActionKey, Keys> keyMapping;
+        private KeyboardState oldState;
+        private Keys lastKey;
+        private int lastKeyAccumulator;
+        private int lastKeyRepeatAccumulator;
+
+        public MouseState MouseState { get { return Mouse.GetState(); } }
+        public bool UseEvents { get; set; }
+        public IInputElement FocusedElement { get; set; }
 
         public InputManager()
         {
             keyMapping = new Dictionary<ActionKey, Keys>();
+            UseEvents = false;
         }
 
         public bool IsKeyDown(ActionKey key)
         {
-            return Keyboard.GetState().IsKeyDown(keyMapping[key]);
+            Keys value;
+            keyMapping.TryGetValue(key, out value);
+
+            if (value == Keys.None)
+                return false;
+
+            return Keyboard.GetState().IsKeyDown(value);
         }
 
         public bool IsKeyUp(ActionKey key)
@@ -30,6 +46,57 @@ namespace DestroyNobots.Engine.Input
             return new Vector2(position.X, position.Y);
         }
 
-        public MouseState MouseState { get { return Mouse.GetState(); } }
+        public void Update(GameTime gt)
+        {
+            if (UseEvents)
+            {
+                KeyboardState newState = Keyboard.GetState();
+
+                if (lastKeyAccumulator < 400)
+                {
+                    lastKeyAccumulator += (int)gt.ElapsedGameTime.TotalMilliseconds;
+                    lastKeyRepeatAccumulator = 0;
+                }
+                else if(lastKey != Keys.None)
+                {
+                    lastKeyRepeatAccumulator += (int)gt.ElapsedGameTime.TotalMilliseconds;
+
+                    while (lastKeyRepeatAccumulator > 30)
+                    {
+                        FocusedElement.OnKeyDown(new KeyboardEventArgs(lastKey, newState));
+                        lastKeyRepeatAccumulator -= 30;
+                    }
+                }
+
+                for (int i = 0; i <= 255; i++)
+                {
+                    if (newState.IsKeyDown((Keys)i))
+                    {
+                        if (!oldState.IsKeyDown((Keys)i))
+                        {
+                            lastKeyAccumulator = 0;
+                            lastKeyRepeatAccumulator = 0;
+                            lastKey = (Keys)i;
+                            FocusedElement.OnKeyDown(new KeyboardEventArgs((Keys)i, newState));
+                        }
+                    }
+                    else if (oldState.IsKeyDown((Keys)i))
+                    {
+                        if ((Keys)i == lastKey)
+                        {
+                            lastKeyAccumulator = 0;
+                            lastKeyRepeatAccumulator = 0;
+                            lastKey = Keys.None;
+                        }
+
+                        FocusedElement.OnKeyUp(new KeyboardEventArgs((Keys)i, newState));
+                    }
+                }
+
+                oldState = newState;
+            }
+        }
+
+
     }
 }
---
+++ DestroyNobots/Engine/Input/KeyboardEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace DestroyNobots.Engine.Input
+{
+    public class KeyboardEventArgs : EventArgs
+    {
+        public byte KeyCode { get { return (byte)Key; } }
+        public Keys Key { get; private set; }
+        public KeyboardState State { get; private set; }
+
+        public bool Shift { get { return State.IsKeyDown(Keys.LeftShift) || State.IsKeyDown(Keys.RightShift); } }
+        public bool Control { get { return State.IsKeyDown(Keys.LeftControl) || State.IsKeyDown(Keys.RightControl); } }
+        public bool CapsLock { get { return State.CapsLock; } }
+
+        public KeyboardEventArgs(Keys key, KeyboardState state)
+        {
+            Key = key;
+            State = state;
+        }
+    }
+}
