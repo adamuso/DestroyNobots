3a86879458073eee1e83339c1a46e12c842b6e54
Adam <adamek.og@gmail.com>
2017-06-15 01:37:02 +0200

    + added physics + added collision detection with SAT

---
+++ DestroyNobots/Engine/Entities/StaticCollider.cs
@@ -0,0 +1,11 @@
+﻿namespace DestroyNobots.Engine.Entities
+{
+    public class StaticCollider : Collider
+    {
+        public override bool CanMove { get { return false; } }
+
+        protected override void MakeDirty() { }
+
+        protected override void MakeClean() { }
+    }
+}
---
+++ DestroyNobots/Engine/Physics/IPhysics.cs
@@ -0,0 +1,11 @@
+﻿using DestroyNobots.Engine.Entities;
+using Microsoft.Xna.Framework;
+
+namespace DestroyNobots.Engine.Physics
+{
+    public interface IPhysics : IUpdateable
+    {
+        void Apply(Collider collider);
+        bool IsDirty { get; }
+    }
+}
